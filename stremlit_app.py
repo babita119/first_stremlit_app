98942f2b80f6486627481beee42016630a5a9b52
babita119 <136703056+babita119@users.noreply.github.com>
2023-06-15 16:37:26 +0530

    Update stremlit_app.py

--- stremlit_app.py
+++ stremlit_app.py
@@ -2,8 +2,10 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
