939e5f5c874668b021366f3473f86f9c4cf3c957
babita119 <136703056+babita119@users.noreply.github.com>
2023-06-16 16:34:35 +0530

    Update stremlit_app.py

--- stremlit_app.py
+++ stremlit_app.py
@@ -36,7 +36,7 @@ try:
 #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Display table on the page
 #streamlit.dataframe(fruityvice_normalized)
-fruit_choice = streamlit.text_input('What fruit would you like information about?)
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
 if not fruit_choice:
 streamlit.error("Please select a fruit to get information.")
 else:  
